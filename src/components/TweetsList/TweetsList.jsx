87642211f2349f7ea793f70616c78a05bc5b16ea
Ira Akulova <ira.akulova2019@gmail.com>
2023-05-09 19:00:46 +0300

    added go-back button

--- src/components/TweetsList/TweetsList.jsx
+++ src/components/TweetsList/TweetsList.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useLocation, Link } from "react-router-dom";
 import { TweetItem } from "../TweeeItem/TweetItem";
 import css from "../TweetsList/TweetList.module.css";
 
@@ -8,6 +9,9 @@ axios.defaults.baseURL = "https://6458af6a8badff578ef7b7c1.mockapi.io/users";
 export const TweetList = () => {
   const [cards, setCards] = useState([]);
   const [page, setPage] = useState(1);
+
+  const location = useLocation();
+  const backLinkHref = useRef(location.state?.from ?? "/");
 
   const fetchUsers = (page) => {
     const params = new URLSearchParams({
@@ -30,6 +34,9 @@ export const TweetList = () => {
     useEffect(() => fetchUsers(page), [page]);
   return (
     <main>
+      <Link to={backLinkHref.current} className={css.backBtn}>
+        Back
+      </Link>
       <ul className={css.grid}>
         {cards.map((card) => (
           <li key={card.id}>
@@ -38,7 +45,9 @@ export const TweetList = () => {
         ))}
       </ul>
       {cards.length > 0 && (
-        <button onClick={buttonClickHandler} className={css.button}>Load more</button>
+        <button onClick={buttonClickHandler} className={css.button}>
+          Load more
+        </button>
       )}
     </main>
   );
